ef5ea6aa78bea8ffcf12c7c9c9b7d860b17002d6
cecilelecerf <c.lecerf.cecile@gmail.com>
2024-02-26 20:02:47 +0100

    fix switch for dark mode

--- src/src/pages/recapToday.js
+++ src/src/pages/recapToday.js
@@ -1,23 +1,20 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import NavLocation from "../components/NavLocation/NavLocation"
 import EmployeeList from "../components/EmployeeList/EmployeeList"
 import Button from "../components/Button/Button"
 import { NavLink } from "react-router-dom"
 import Switch from "../components/Switch/Switch";
-import DarkModeContext from '../components/DarkMode/DarkMode';
 import { getSelectedEmployees } from "../LocalStorageService";
 
 const RecapToday = () => {
-    const salleSelectedEmployees = getSelectedEmployees("salle");
-    const cuisineSelectedEmployees = getSelectedEmployees("cuisine");
-    const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
-    const selectedEmployees = [...salleSelectedEmployees, ...cuisineSelectedEmployees];
+    const selectedEmployees = [...getSelectedEmployees()];
+    console.log(selectedEmployees)
     return (
         <main className="d-flex flex-column justify-content-between">
             <NavLocation defaultLocation= "service" />
             <p className="text-primary fw-bold fs-5 text-center">Employés sélectionnés</p>
-        <EmployeeList height="200px" data={selectedEmployees} />
-            <Switch leftSvg="../components/Switch/LightSun.svg" rightSvg="../components/Switch/LightMoon.svg" onChange={toggleDarkMode} />
+            <EmployeeList height="200px" data={selectedEmployees} resume={true} />
+            <Switch/>
             <NavLink to="/addTips">
                 <Button text="Commancer le service !" />
             </NavLink>
