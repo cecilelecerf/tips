16eb5d4590cb6ade422d13fef9c998f78d0a754c
Diece98 <esther.mehal@gmail.com>
2024-02-26 11:54:35 +0100

    Add  menu animation

--- src/src/App.js
+++ src/src/App.js
@@ -1,28 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
-import BurgerMenu from './components/burgerMenu'; 
+import BurgerMenu from './NavBar/burgerMenu';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-            <BurgerMenu />
-
+        <div className="menu-container">
+          <BurgerMenu />
         </div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/src/NavBar/burgerMenu.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './burgerMenu.css';
+import { ReactComponent as BurgerIcon } from './burgerIcon.svg';
+
+const BurgerMenu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleToggle = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <>
+            <nav className="navbar bg-body-tertiary fixed-top">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Qui travaille aujourd’hui ?</a>
+                    <BurgerIcon onClick={handleToggle} className="navbar-toggler" type="button" aria-controls="offcanvasNavbar" aria-label="Toggle navigation" />
+                    <div className={`offcanvas offcanvas-end ${isOpen ? 'show' : ''}`} tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" style={{ width: '50%' }}>
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" onClick={handleToggle}></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#">Service</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Administrateur</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default BurgerMenu;
