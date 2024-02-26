d0f3fe67389af2428981821db45b0e2824b8bd5b
Diece98 <esther.mehal@gmail.com>
2024-02-26 10:38:06 +0100

    Add base menu

--- src/src/App.js
+++ src/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import BurgerMenu from './components/burgerMenu'; 
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+            <BurgerMenu />
+
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
